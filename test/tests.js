4e9c40afef2cba1fd2191a48582e3504bf2dca68
e2tha-e <ee@electric-eloquence.io>
2019-03-19 15:11:06 -0400

    minor syntax update in test

--- test/tests.js
+++ test/tests.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const expect = require('chai').expect;
+const {expect} = require('chai');
 const gulp = require('gulp');
 
 const runSequence = require('../index');
